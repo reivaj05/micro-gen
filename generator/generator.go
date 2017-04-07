25be9104536575cc6194cc4f17067c5c1b8dd6c2
Javier Adriel Sanchez <reivaj0705@gmail.com>
2017-04-07 16:59:41 -0500

    Write generator skeleton code

--- generator/generator.go
+++ generator/generator.go
@@ -2,7 +2,42 @@ package generator
 
 import "fmt"
 
+type generator func() error
+
+var generators = map[string]generator{
+	"go":         generateGo,
+	"python":     generatePython,
+	"ruby":       generateRuby,
+	"javascript": generateJS,
+	"rust":       generateRust,
+}
+
 func Generate(args ...string) error {
-	fmt.Println("TODO: Implement generate")
+	language := args[0]
+	return generators[language]()
+}
+
+func generateGo() error {
+	fmt.Println("TODO: Implement go generator")
+	return nil
+}
+
+func generatePython() error {
+	fmt.Println("TODO: Implement python generator")
+	return nil
+}
+
+func generateRuby() error {
+	fmt.Println("TODO: Implement ruby generator")
+	return nil
+}
+
+func generateJS() error {
+	fmt.Println("TODO: Implement js generator")
+	return nil
+}
+
+func generateRust() error {
+	fmt.Println("TODO: Implement rust generator")
 	return nil
 }
