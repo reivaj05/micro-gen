7e512381362d9b9d52e1ec65a62f387041f856bd
Javier Adriel Sanchez <reivaj0705@gmail.com>
2017-04-10 19:04:11 -0500

    Files renamed

--- generator/generator.go
+++ generator/generator.go
@@ -1,11 +1,11 @@
 package generator
 
 import (
-	goGen "github.com/reivaj05/micro-gen/generator/generators/go"
-	jsGen "github.com/reivaj05/micro-gen/generator/generators/javascript"
-	pythonGen "github.com/reivaj05/micro-gen/generator/generators/python"
-	rubyGen "github.com/reivaj05/micro-gen/generator/generators/ruby"
-	rustGen "github.com/reivaj05/micro-gen/generator/generators/rust"
+	goBuilder "github.com/reivaj05/micro-gen/generator/builders/go"
+	jsBuilder "github.com/reivaj05/micro-gen/generator/builders/javascript"
+	pythonBuilder "github.com/reivaj05/micro-gen/generator/builders/python"
+	rubyBuilder "github.com/reivaj05/micro-gen/generator/builders/ruby"
+	rustBuilder "github.com/reivaj05/micro-gen/generator/builders/rust"
 )
 
 type generator func() error
@@ -24,23 +24,23 @@ func Generate(args ...string) error {
 }
 
 func generateGo() error {
-	return goGen.Build()
+	return goBuilder.Build()
 }
 
 func generateJS() error {
-	return jsGen.Build()
+	return jsBuilder.Build()
 }
 
 func generatePython() error {
-	return pythonGen.Build()
+	return pythonBuilder.Build()
 }
 
 func generateRuby() error {
-	return rubyGen.Build()
+	return rubyBuilder.Build()
 }
 
 func generateRust() error {
-	return rustGen.Build()
+	return rustBuilder.Build()
 }
 
 func rollback() error {
