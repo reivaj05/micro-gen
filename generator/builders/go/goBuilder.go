4051b32f6bbf442771894bcfea86099e8dcd921b
Javier Adriel Sanchez <reivaj0705@gmail.com>
2017-04-10 20:07:19 -0500

    Generate main file

--- generator/builders/go/goBuilder.go
+++ generator/builders/go/goBuilder.go
@@ -1,123 +1,77 @@
 package goGenerator
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"text/template"
+
+	"github.com/reivaj05/micro-gen/generator/utils"
+)
+
+type data struct {
+	ServiceName string
+}
 
 func Build(serviceName string) error {
+	// path := utils.GetMicroGenPath()
 	fmt.Println("TODO: Implement go generator")
+	if err := createServiceDir(serviceName); err != nil {
+		return err
+	}
+	return createService(serviceName)
+}
+
+func createServiceDir(serviceName string) error {
+	dst := fmt.Sprintf("./%s", serviceName)
+	return os.MkdirAll(dst, os.ModePerm)
+}
+
+func createService(serviceName string) error {
+	return generateFiles(serviceName)
+}
+
+func generateFiles(serviceName string) error {
+	if err := generateMainFile(serviceName); err != nil {
+		return err
+	}
+	// if err := generateServiceFile(path, serviceName); err != nil {
+	// 	return err
+	// }
+	// if err := generateProtoFiles(path, serviceName); err != nil {
+	// 	return err
+	// }
 	return nil
 }
 
-// func createService(args ...string) {
-// 	// TODO: Create properly template files (protos and go files for services)
-// 	// TODO: Implement rest of template files
-// 	basePath := joinPath()
-// 	for _, serviceName := range args {
-// 		if err := generateFiles(basePath, serviceName); err != nil {
-// 			fmt.Errorf("Service not created: " + err.Error())
-// 			rollback(serviceName)
-// 		}
-// 	}
+func generateMainFile(serviceName string) error {
+	return _generateFile(&utils.GenerateOptions{
+		ServiceName:   serviceName,
+		FileName:      "main",
+		FileExtension: "go",
+		FileTemplate:  "main.gen",
+		Data: &data{
+			ServiceName: serviceName,
+		},
+	})
+}
 
-// }
+func _generateFile(options *utils.GenerateOptions) error {
+	file, err := _createFile(options)
+	if err != nil {
+		return err
+	}
+	return _writeTemplateContent(file, options)
+}
 
-// func joinPath() string {
-// 	const relativePath = "/src/github.com/reivaj05/apigateway"
-// 	goPath := os.Getenv("GOPATH")
-// 	return goPath + relativePath
-// }
+func _createFile(options *utils.GenerateOptions) (*os.File, error) {
+	dst := fmt.Sprintf("./%s/%s.%s", options.ServiceName,
+		options.FileName, options.FileExtension)
+	return os.Create(dst)
+}
 
-// func generateFiles(path, serviceName string) error {
-// 	if err := generateAPIFile(path, serviceName); err != nil {
-// 		return err
-// 	}
-// 	if err := generateServiceFile(path, serviceName); err != nil {
-// 		return err
-// 	}
-// 	if err := generateProtoFiles(path, serviceName); err != nil {
-// 		return err
-// 	}
-// 	return nil
-// }
-
-// func generateAPIFile(path, serviceName string) error {
-// 	path += "/api/" + serviceName + "/"
-// 	return _generateFile(&generateOptions{
-// 		path:          path,
-// 		serviceName:   serviceName,
-// 		fileExtension: ".go",
-// 		fileTemplate:  "goAPI.txt",
-// 		data: &goAPITemplateData{
-// 			ServiceName:      serviceName,
-// 			UpperServiceName: inflect.Titleize(serviceName),
-// 		},
-// 	})
-
-// }
-
-// func generateServiceFile(path, serviceName string) error {
-// 	path += "/services/" + serviceName + "/"
-// 	return _generateFile(&generateOptions{
-// 		path:          path,
-// 		serviceName:   serviceName,
-// 		fileExtension: ".go",
-// 		fileTemplate:  "goService.txt",
-// 		data: &goAPITemplateData{
-// 			ServiceName:      serviceName,
-// 			UpperServiceName: inflect.Titleize(serviceName),
-// 		},
-// 	})
-// }
-
-// func generateProtoFiles(path, serviceName string) error {
-// 	sp := camelcase.Split(serviceName)[0]
-// 	if err := _generateFile(&generateOptions{
-// 		path:          path + "/protos/api/",
-// 		serviceName:   serviceName,
-// 		fileExtension: ".proto",
-// 		fileTemplate:  "protoAPI.txt",
-// 		data: &protoAPITemplateData{
-// 			ServiceName:      serviceName,
-// 			ResourcePath:     sp,
-// 			UpperServiceName: inflect.Titleize(serviceName),
-// 		},
-// 	}); err != nil {
-// 		return err
-// 	}
-// 	return _generateFile(&generateOptions{
-// 		path:          path + "/protos/services/",
-// 		serviceName:   serviceName,
-// 		fileExtension: ".proto",
-// 		fileTemplate:  "protoService.txt",
-// 		data: struct {
-// 			ServiceName      string
-// 			UpperServiceName string
-// 		}{
-// 			ServiceName:      serviceName,
-// 			UpperServiceName: inflect.Titleize(serviceName),
-// 		},
-// 	})
-// }
-
-// func _generateFile(options *generateOptions) error {
-// 	file, err := _createFile(options)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	return _writeTemplateContent(file, options)
-// }
-
-// func _createFile(options *generateOptions) (*os.File, error) {
-// 	err := os.MkdirAll(options.path, os.ModePerm)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return os.Create(options.getFilePath())
-// }
-
-// func _writeTemplateContent(file *os.File, options *generateOptions) error {
-// 	defer file.Close()
-// 	tmpl := template.Must(template.ParseFiles(
-// 		GoConfig.GetConfigStringValue("templatesPath") + options.fileTemplate),
-// 	)
-// 	return tmpl.Execute(file, options.data)
-// }
+func _writeTemplateContent(file *os.File, options *utils.GenerateOptions) error {
+	defer file.Close()
+	templateDir := fmt.Sprintf("%s/generator/builders/go/templates/%s", utils.GetMicroGenPath(), options.FileTemplate)
+	tmpl := template.Must(template.ParseFiles(templateDir))
+	return tmpl.Execute(file, options.Data)
+}
