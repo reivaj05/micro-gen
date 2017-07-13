5dd00ca03496bee9e17e42e2c9938904b4ceac54
Javier Adriel Sanchez <reivaj0705@gmail.com>
2017-07-12 20:29:52 -0500

    Create python service directory

--- generator/builders/python/pythonBuilder.go
+++ generator/builders/python/pythonBuilder.go
@@ -1,8 +1,24 @@
 package pythonGenerator
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func Build(serviceName string) error {
 	fmt.Println("TODO: Implement python generator")
+	if err := createServiceDir(serviceName); err != nil {
+		return err
+	}
+	return createService(serviceName)
+}
+
+func createServiceDir(serviceName string) error {
+	dst := fmt.Sprintf("./%s", serviceName)
+	return os.MkdirAll(dst, os.ModePerm)
+}
+
+func createService(serviceName string) error {
+	fmt.Println("TODO: Implement create service function")
 	return nil
 }
