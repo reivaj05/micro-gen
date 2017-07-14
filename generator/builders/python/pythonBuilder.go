8c0642ba60694982c52c2e82a9587afd2a37adf5
Javier Adriel Sanchez <reivaj0705@gmail.com>
2017-07-13 19:07:03 -0500

    Django project gen files

--- generator/builders/python/pythonBuilder.go
+++ generator/builders/python/pythonBuilder.go
@@ -13,6 +13,7 @@ func Build(serviceName string) error {
 	return createService(serviceName)
 }
 
+// TODO: Refactor to one function(all builders create a new directory)
 func createServiceDir(serviceName string) error {
 	dst := fmt.Sprintf("./%s", serviceName)
 	return os.MkdirAll(dst, os.ModePerm)
