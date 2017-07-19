b9ca6022bc3eb57eb678107bb9c173319f491dce
Javier Adriel Sanchez <reivaj0705@gmail.com>
2017-07-19 17:59:51 -0500

    Delete mockService directory generate by tests

--- generator/builders/python/mockService/app/views.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse('TODO: Change for a complete microservice')
