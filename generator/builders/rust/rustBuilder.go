00e2f835f684d31648bbb311d32aa6cdc115574f
reivaj05 <reivaj0705@gmail.com>
2017-11-14 17:30:28 -0600

    Add skeleton for rust builder

--- generator/builders/rust/rustBuilder.go
+++ generator/builders/rust/rustBuilder.go
@@ -3,6 +3,44 @@ package rustGenerator
 import "fmt"
 
 func Build(serviceName string) error {
-	fmt.Println("TODO: Implement rust generator")
+	if err := createDirectories(serviceName); err != nil {
+		return err
+	}
+	return createService(serviceName)
+}
+
+func createDirectories(serviceName string) error {
+	paths := []string{
+		fmt.Sprintf("./%s", serviceName), fmt.Sprintf("./%s/config", serviceName),
+		fmt.Sprintf("./%s/app", serviceName), fmt.Sprintf("./%s/scripts", serviceName),
+	}
+	for _, path := range paths {
+		if err := utils.CreateDir(path); err != nil {
+			return err
+		}	
+	}
+	return nil
+}
+
+func createService(serviceName string) error {
+	return generateAllFiles(serviceName)
+}
+
+func generateAllFiles(serviceName string) error {
+	for _, optionsList := range [][]*utils.GenerateFileOptions{
+		configFileOptions, appFileOptions, buildFileOptions, scriptFileOptions}{
+		if err := generateFilesWithOptionsList(serviceName, optionsList); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func generateFilesWithOptionsList(serviceName string, fileOptions []*utils.GenerateFileOptions) error {
+	for _, options := range fileOptions {
+		if err := utils.GenerateFile(serviceName, options); err != nil {
+			return err
+		}
+	}
 	return nil
 }
