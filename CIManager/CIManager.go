65cf1f11d88ceabd71d043fbbf590b3282e7f6fc
reivaj05 <reivaj0705@gmail.com>
2017-12-13 15:38:21 -0600

    Add skeleton functions for CI connectors

--- CIManager/CIManager.go
+++ CIManager/CIManager.go
@@ -24,41 +24,35 @@ var CIConnectors = map[string]CIConnector{
 }
 
 
-func ConnectWithCIProvider(provider string) error {
-	if creator, ok := repoProviders[provider]; ok {
-		return creator(serviceName)
+func ConnectWithCIProvider(serviceName, provider string) error {
+	if connector, ok := CIConnectors[provider]; ok {
+		return connector(serviceName)
 	}
-	return fmt.Errorf("Repo provider '%s' not supported", provider)
+	return fmt.Errorf("CI provider '%s' not supported", provider)
 }
 
-func createLocalRepo(serviceName string) error {
-	fmt.Println("Creating local repo...")
-	cmd := exec.Command("sh", "-c", fmt.Sprintf("cd %s && git init", serviceName))
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return err
-	}
-	fmt.Println(string(output))
-	return nil
-}
-
-func createGithubRepo(serviceName string) error {
-	fmt.Printf("Creating %s github repository...\n", serviceName)
-	ctx := context.Background()
-	client, err := createGitHubClient(ctx)
-	if err != nil {
-		return err
-	}
-	return __createGithubRepo(serviceName, client, ctx)
-}
-
-func createGitHubClient(ctx context.Context) (*github.Client, error) {
-	accessToken, err := getToken(githubTokenKey)
+func connectTravis(serviceName string) error {
+	accessToken, err := getToken(travisCIKey)
 	if err != nil {
 		return nil, err
 	}
-	tc := oauth2.NewClient(ctx, oauth2.StaticTokenSource(&oauth2.Token{AccessToken: accessToken}))
-	return github.NewClient(tc), nil
+	return nil
+}
+
+func connectJenkins(serviceName string) error {
+	accessToken, err := getToken(jenkinsCIKey)
+	if err != nil {
+		return nil, err
+	}
+	return nil
+}
+
+func connectCircle(serviceName string) error {
+	accessToken, err := getToken(circleCIKey)
+	if err != nil {
+		return nil, err
+	}
+	return nil
 }
 
 func getToken(key string) (string, error) {
@@ -67,32 +61,4 @@ func getToken(key string) (string, error) {
 		return "", fmt.Errorf("%s env var does not exist", key)
 	}
 	return accessToken, nil
-}
-
-func __createGithubRepo(serviceName string, client *github.Client,
-	ctx context.Context) (err error) {
-
-	repo := &github.Repository{Name:    github.String(serviceName)}
-	repo, _, err  = client.Repositories.Create(ctx, "", repo)
-	if err != nil {
-		return err
-	}
-	return linkGithubRepoToLocalRepo(repo, serviceName)
-}
-
-func linkGithubRepoToLocalRepo(repo *github.Repository, serviceName string) error {
-	fmt.Printf("Linking %s repository to local repository...\n", serviceName)
-	cmd := exec.Command("sh", "-c", fmt.Sprintf("cd %s && git remote add origin %s", serviceName, *repo.SSHURL))
-	_, err := cmd.CombinedOutput()
-	return err
-}
-
-func createBitbucketRepo(serviceName string) error {
-	fmt.Println("TODO: Create bitbucket repo")
-	return nil
-}
-
-func createGitlabRepo(serviceName string) error {
-	fmt.Println("TODO: Create gitlab repo")
-	return nil
 }
