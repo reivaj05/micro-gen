c01ea70b74e27ab771a34e45f7f6b10d4eb93335
reivaj05 <reivaj0705@gmail.com>
2017-12-15 15:37:02 -0600

    Add test request to get travis repos

--- CIManager/CIManager.go
+++ CIManager/CIManager.go
@@ -3,6 +3,8 @@ package CIManager
 import (
 	"fmt"
 	"os"
+
+	"github.com/reivaj05/GoRequester"
 )
 
 type CIConnector func(string) error
@@ -10,6 +12,10 @@ type CIConnector func(string) error
 var travisCIKey = "TRAVIS_TOKEN"
 var jenkinsCIKey = "JENKINS_TOKEN"
 var circleCIKey = "CIRCLE_TOKEN"
+
+var requesterObj = requester.New()
+
+var travisActivateEndpoint = "https://api.travis-ci.org/repos"
 
 var CIConnectors = map[string]CIConnector{
 	"travis":  travisConnector,
@@ -25,11 +31,32 @@ func ConnectWithCIProvider(serviceName, provider string) error {
 }
 
 func travisConnector(serviceName string) error {
-	_, err := getToken(travisCIKey)
+	token, err := getToken(travisCIKey)
 	if err != nil {
 		return err
 	}
+	return activateRepoInTravis(token)
+}
+
+func activateRepoInTravis(token string) error {
+	response, status_code, err := requesterObj.MakeRequest(createTravisRequestConfig(token))
+	fmt.Println(response, status_code, err)
 	return nil
+}
+
+func createTravisRequestConfig(token string) *requester.RequestConfig {
+	return &requester.RequestConfig{
+		Method:  "GET",
+		URL:     travisActivateEndpoint,
+		Headers: createTravisRequestHeaders(token),
+	}
+}
+
+func createTravisRequestHeaders(token string) map[string]string {
+	return map[string]string{
+		"Travis-API-Version": "3",
+		"Authorization":      fmt.Sprintf("token %s", token),
+	}
 }
 
 func jenkinsConnector(serviceName string) error {
