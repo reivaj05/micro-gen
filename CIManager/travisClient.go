6d9240cc4f428bfc0cc3e79c63c543a0e0635093
reivaj05 <reivaj0705@gmail.com>
2017-12-15 16:10:42 -0600

    Add travis client file

---
+++ CIManager/travisClient.go
@@ -0,0 +1,32 @@
+package CIManager
+
+import (
+	"github.com/reivaj05/GoRequester"
+)
+
+type travisClient struct {
+	requesterObj *requester.Requester
+	token        string
+	headers      map[string]string
+}
+
+func New() *travisClient {
+	return &travisClient{
+	// TODO:
+	}
+}
+
+func (client *travisClient) enableRepo(repoName string) (string, error) {
+	// TODO:
+	return "", nil
+}
+
+func (client *travisClient) getRepos() (string, error) {
+	// TODO:
+	return "", nil
+}
+
+func (client *travisClient) syncAccount() (string, error) {
+	// TODO:
+	return "", nil
+}
