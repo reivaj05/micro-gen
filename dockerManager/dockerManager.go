aa4f3366fb66e58a672706e77fd27deb21f58363
reivaj05 <reivaj0705@gmail.com>
2017-12-20 18:54:09 -0600

    Add dockerClient struct

--- dockerManager/dockerManager.go
+++ dockerManager/dockerManager.go
@@ -1,1 +1,15 @@
 package dockerManager
+
+import requester "github.com/reivaj05/GoRequester"
+
+type dockerClient struct {
+	requesterObj *requester.Requester
+	token        string
+}
+
+func NewDockerClient(token string) *dockerClient {
+	return &dockerClient{
+		requesterObj: requester.New(),
+		token:        token,
+	}
+}
