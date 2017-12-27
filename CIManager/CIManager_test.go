f333688bf4cf2d9df631fcdc0c1199000ed4c7e8
reivaj05 <reivaj0705@gmail.com>
2017-12-27 16:14:53 -0600

    Add travis skeleton tests

--- CIManager/CIManager_test.go
+++ CIManager/CIManager_test.go
@@ -1,1 +1,57 @@
 package CIManager
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type CIManagerTestSuite struct {
+	suite.Suite
+	assert          *assert.Assertions
+	serviceName     string
+	travisProvider  string
+	jenkinsProvider string
+	circleProvider  string
+	travisToken     string
+}
+
+func (suite *CIManagerTestSuite) SetupSuite() {
+	suite.assert = assert.New(suite.T())
+	suite.serviceName = "mockService"
+	suite.travisProvider = "travis"
+	suite.jenkinsProvider = "jenkins"
+	suite.circleProvider = "circle"
+}
+
+func (suite *CIManagerTestSuite) SetupTest() {
+	suite.travisToken = os.Getenv(travisCIKey)
+}
+
+func (suite *CIManagerTestSuite) TearDownTest() {
+	os.Setenv(travisCIKey, suite.travisToken)
+}
+
+func (suite *CIManagerTestSuite) TestConnectWithTravisProviderSuccessfully() {
+	os.Setenv(travisCIKey, "TRAVIS_MOCK_TOKEN")
+	err := ConnectWithCIProvider(suite.serviceName, suite.travisProvider)
+	suite.assert.Nil(err)
+}
+
+func (suite *CIManagerTestSuite) TestConnectWithTravisProviderUnsuccessfully() {
+	os.Setenv(travisCIKey, "TRAVIS_MOCK_TOKEN")
+	err := ConnectWithCIProvider(suite.serviceName, suite.travisProvider)
+	suite.assert.NotNil(err)
+}
+
+func (suite *CIManagerTestSuite) TestConnectWithTravisProviderWithouToken() {
+	os.Setenv(travisCIKey, "")
+	err := ConnectWithCIProvider(suite.serviceName, suite.travisProvider)
+	suite.assert.NotNil(err)
+}
+
+func TestCIManager(t *testing.T) {
+	suite.Run(t, new(CIManagerTestSuite))
+}
