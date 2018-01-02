8e208a6f7d24845657fcd49e801a9a985ef132cc
reivaj05 <reivaj0705@gmail.com>
2018-01-02 17:05:18 -0600

    Add travisClient_test skeleton

--- CIManager/travisClient_test.go
+++ CIManager/travisClient_test.go
@@ -1,1 +1,39 @@
 package CIManager
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type TravisClientTestSuite struct {
+	suite.Suite
+	assert      *assert.Assertions
+	serviceName string
+	token       string
+}
+
+func (suite *TravisClientTestSuite) SetupSuite() {
+	suite.assert = assert.New(suite.T())
+	suite.token = "mockToken"
+	suite.serviceName = "mockServiceName"
+}
+
+func (suite *TravisClientTestSuite) SetupTest() {
+	//
+}
+
+func (suite *TravisClientTestSuite) TearDownTest() {
+	//
+}
+
+func (suite *TravisClientTestSuite) TestNewTravisClient() {
+	//
+}
+
+func TestTravisClient(t *testing.T) {
+	suite.Run(t, new(TravisClientTestSuite))
+}
