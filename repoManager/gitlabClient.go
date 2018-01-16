6755f8adce1d7059748b8f61f7baf64e3b2d774b
Javier Adriel Sanchez <reivaj0705@gmail.com>
2018-01-16 16:54:45 -0600

    Create Gitlab project

--- repoManager/gitlabClient.go
+++ repoManager/gitlabClient.go
@@ -1,6 +1,10 @@
 package repoManager
 
-import "fmt"
+import (
+	"github.com/xanzy/go-gitlab"
+
+	"fmt"
+)
 
 type gitlabClient struct {
 	token string
@@ -13,6 +17,27 @@ func NewGitlabClient(token string) repoProviderClient {
 }
 
 func (client *gitlabClient) CreateCloudRepo(serviceName string) (string, error) {
-	fmt.Println("TODO: Implement gitlab client")
-	return "", nil
+	gClient := gitlab.NewClient(nil, client.token)
+	options := client.createProjectOptions(serviceName)
+	return client.createGitlabProject(gClient, options)
+}
+
+func (client *gitlabClient) createGitlabProject(
+	gClient *gitlab.Client, options *gitlab.CreateProjectOptions) (string, error) {
+
+	project, _, err := gClient.Projects.CreateProject(options)
+	if err != nil {
+		return "", err
+	}
+	return project.SSHURLToRepo, nil
+}
+
+func (client *gitlabClient) createProjectOptions(serviceName string) *gitlab.CreateProjectOptions {
+	return &gitlab.CreateProjectOptions{
+		Name:                 gitlab.String(serviceName),
+		Description:          gitlab.String(fmt.Sprintf("Project %s created with micro-gen", serviceName)),
+		MergeRequestsEnabled: gitlab.Bool(true),
+		SnippetsEnabled:      gitlab.Bool(true),
+		Visibility:           gitlab.Visibility(gitlab.PublicVisibility),
+	}
 }
