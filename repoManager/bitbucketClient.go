0200018424a9194c75d06ca4b3e79e613e9ded3c
Javier Adriel Sanchez <reivaj0705@gmail.com>
2018-01-15 18:14:14 -0600

    Add dummy bitbucket client

---
+++ repoManager/bitbucketClient.go
@@ -0,0 +1,18 @@
+package repoManager
+
+import "fmt"
+
+type bitbucketClient struct {
+	token string
+}
+
+func NewBitbucketClient(token string) repoProviderClient {
+	return &bitbucketClient{
+		token: token,
+	}
+}
+
+func (client *bitbucketClient) CreateCloudRepo(serviceName string) (string, error) {
+	fmt.Println("TODO: Implement gitlab client")
+	return "", nil
+}
