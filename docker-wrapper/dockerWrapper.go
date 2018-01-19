b65e0cfccafa03e0bb34f43a88f7cde572c5698a
Javier Adriel Sanchez <reivaj0705@gmail.com>
2018-01-19 16:12:28 -0600

    Add dockerWrapper skeleton

--- docker-wrapper/dockerWrapper.go
+++ docker-wrapper/dockerWrapper.go
@@ -1,20 +1,45 @@
 package dockerWrapper
 
 import (
-	"github.com/reivaj05/GoJSON"
+	"encoding/base64"
+	"encoding/json"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"golang.org/x/net/context"
 )
 
-func (client *dockerClient) getRelatedImages(serviceName string) []string {
-	// TODO: Implement
-	return []string{}
+type dockerManager struct {
+	cli   *client.Client
+	ctx   context.Context
+	token string
 }
 
-func (client *dockerClient) filterImagesByName(serviceName string) []*GoJSON.JSONWrapper {
-	// TODO: Implement
-	return nil
+func NewDockerManager() (*dockerManager, error) {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		return nil, err
+	}
+	token, err := getAuthToken(cli)
+	if err != nil {
+		return nil, err
+	}
+	return &dockerManager{cli: cli, ctx: context.Background(), token: token}, nil
 }
 
-func (client *dockerClient) getImages() []*GoJSON.JSONWrapper {
-	// TODO: Implement
-	return nil
+func getAuthToken(cli *client.Client) (string, error) {
+	authConfig := types.AuthConfig{
+		Username: "username",
+		Password: "password",
+	}
+	encodedJSON, err := json.Marshal(authConfig)
+	if err != nil {
+		panic(err)
+	}
+	authStr := base64.URLEncoding.EncodeToString(encodedJSON)
+
+}
+
+func (manager *dockerManager) test() {
+
 }
