c38af20b74eeba9260c68e098657106868b64145
reivaj05 <reivaj0705@gmail.com>
2018-01-22 17:44:01 -0600

    Add dummy/ugly/dirty login(needs refactor)

--- docker-wrapper/dockerWrapper.go
+++ docker-wrapper/dockerWrapper.go
@@ -4,12 +4,18 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/reivaj05/GoJSON"
 	"github.com/reivaj05/GoRequester"
 )
+
+// TODO: Refactor
 
 var dockerUsernameKey = "DOCKER_USERNAME"
 var dockerPasswordKey = "DOCKER_PASSWORD"
 var dockerRegistryHostKey = "DOCKER_REGISTRY_HOST"
+
+var repositoriesEndpoint = "%s/v2/repositories/%s/"
+var loginEndpoint = "%s/v2/users/login/"
 
 type dockerRegistryManager struct {
 	client   *requester.Requester
@@ -19,31 +25,18 @@ type dockerRegistryManager struct {
 }
 
 func NewDockerRegistryManager() (*dockerRegistryManager, error) {
-	if err := checkDockerRegistryCredentials(); err != nil {
+	if err := checkDockerCredentials(); err != nil {
 		return nil, err
 	}
-	return &dockerRegistryManager{
-		token:    getToken(),
+	manager := &dockerRegistryManager{
 		host:     os.Getenv(dockerRegistryHostKey),
 		username: os.Getenv(dockerUsernameKey),
 		client:   requester.New(),
-	}, nil
-}
-
-func checkDockerRegistryCredentials() error {
-	if err := checkDockerCredentials(); err != nil {
-		return err
 	}
-	if value := os.Getenv(dockerRegistryHostKey); value == "" {
-		return fmt.Errorf("Env var %s not set", dockerRegistryHostKey)
+	if err := manager.setToken(); err != nil {
+		return nil, err
 	}
-	return nil
-}
-
-func getToken() string {
-	username, password := os.Getenv(dockerUsernameKey), os.Getenv(dockerPasswordKey)
-	jsonString := fmt.Sprintf(`{"username":"%s","password":"%s"}`, username, password)
-	return base64.URLEncoding.EncodeToString([]byte(jsonString))
+	return manager, nil
 }
 
 func checkDockerCredentials() error {
@@ -55,11 +48,49 @@ func checkDockerCredentials() error {
 	return nil
 }
 
+func (manager *dockerRegistryManager) setToken() error {
+	token, err := manager.getToken()
+	if err != nil {
+		return err
+	}
+	manager.token = token
+	return nil
+}
+
+func (manager *dockerRegistryManager) getToken() (string, error) {
+	username, password := os.Getenv(dockerUsernameKey), os.Getenv(dockerPasswordKey)
+	data := fmt.Sprintf(`{"username":"%s","password":"%s"}`, username, password)
+	return manager.login(data)
+}
+
+func (manager *dockerRegistryManager) login(data string) (string, error) {
+	response, status, err := manager.client.MakeRequest(manager.createLoginRequestConfig(data))
+	if err != nil || status > 400 {
+		return "", fmt.Errorf("Error %v: Got status %d", status, err)
+	}
+	return manager.parseLoginResponse(response)
+}
+
+func (manager *dockerRegistryManager) createLoginRequestConfig(data string) *requester.RequestConfig {
+	return &requester.RequestConfig{
+		URL:     fmt.Sprintf(loginEndpoint, manager.host),
+		Method:  "POST",
+		Body:    []byte(data),
+		Headers: map[string]string{"Content-Type": "application/json"},
+	}
+}
+
+func (manager *dockerRegistryManager) parseLoginResponse(response string) (string, error) {
+	jsonString, _ := GoJSON.New(response)
+	value, _ := jsonString.GetStringFromPath("token")
+	return value, nil
+}
+
 func (manager *dockerRegistryManager) SearchRepositories() {
 	response, status, err := manager.client.MakeRequest(&requester.RequestConfig{
 		Method:  "GET",
-		URL:     fmt.Sprintf("%s/v2/repositories/%s/", manager.host, manager.username),
-		Headers: map[string]string{"Authorization": " " + manager.token},
+		URL:     fmt.Sprintf(repositoriesEndpoint, manager.host, manager.username),
+		Headers: map[string]string{"Authorization": "JWT " + manager.token},
 	})
 	fmt.Println(response, status, err)
 
