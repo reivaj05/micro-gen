c483c7fc5ccbb0ecabcd6600bc1ccf57bd0e6c97
reivaj05 <reivaj0705@gmail.com>
2018-01-22 16:58:25 -0600

    Remove unused code

--- docker-wrapper/dockerWrapper.go
+++ docker-wrapper/dockerWrapper.go
@@ -1,29 +1,15 @@
 package dockerWrapper
 
 import (
-	"encoding/base64"
-	// "encoding/json"
 	"fmt"
-	// "io"
 	"os"
 
-	// "github.com/docker/docker/api/types"
-	// "github.com/docker/docker/client"
-	// "github.com/heroku/docker-registry-client/registry"
-	// "github.com/reivaj05/GoJSON"
 	"github.com/reivaj05/GoRequester"
-	// "golang.org/x/net/context"
 )
 
 var dockerUsernameKey = "DOCKER_USERNAME"
 var dockerPasswordKey = "DOCKER_PASSWORD"
 var dockerRegistryHostKey = "DOCKER_REGISTRY_HOST"
-
-// type dockerManager struct {
-// 	cli   *client.Client
-// 	ctx   context.Context
-// 	token string
-// }
 
 type dockerRegistryManager struct {
 	client   *requester.Requester
@@ -31,46 +17,6 @@ type dockerRegistryManager struct {
 	host     string
 	username string
 }
-
-// func NewDockerManager() (*dockerManager, error) {
-// 	cli, err := client.NewEnvClient()
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	token, err := getAuthToken(cli)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return &dockerManager{cli: cli, ctx: context.Background(), token: token}, nil
-// }
-
-// func getAuthToken(cli *client.Client) (string, error) {
-// 	if err := checkDockerCredentials(); err != nil {
-// 		return "", err
-// 	}
-// 	encodedJSON, err := getAuthEncoded()
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	return base64.URLEncoding.EncodeToString(encodedJSON), nil
-// }
-
-func checkDockerCredentials() error {
-	for _, key := range []string{dockerUsernameKey, dockerPasswordKey} {
-		if value := os.Getenv(dockerUsernameKey); value == "" {
-			return fmt.Errorf("Env var %s not set", key)
-		}
-	}
-	return nil
-}
-
-// func getAuthEncoded() ([]byte, error) {
-// 	authConfig := types.AuthConfig{
-// 		Username: os.Getenv(dockerUsernameKey),
-// 		Password: os.Getenv(dockerPasswordKey),
-// 	}
-// 	return json.Marshal(authConfig)
-// }
 
 func NewDockerRegistryManager() (*dockerRegistryManager, error) {
 	if err := checkDockerRegistryCredentials(); err != nil {
@@ -84,18 +30,27 @@ func NewDockerRegistryManager() (*dockerRegistryManager, error) {
 	}, nil
 }
 
-func getToken() string {
-	username, password := os.Getenv(dockerUsernameKey), os.Getenv(dockerPasswordKey)
-	jsonString := fmt.Sprintf(`{"username":"%s","password":"%s"}`, username, password)
-	return base64.URLEncoding.EncodeToString([]byte(jsonString))
-}
-
 func checkDockerRegistryCredentials() error {
 	if err := checkDockerCredentials(); err != nil {
 		return err
 	}
 	if value := os.Getenv(dockerRegistryHostKey); value == "" {
 		return fmt.Errorf("Env var %s not set", dockerRegistryHostKey)
+	}
+	return nil
+}
+
+func getToken() string {
+	username, password := os.Getenv(dockerUsernameKey), os.Getenv(dockerPasswordKey)
+	jsonString := fmt.Sprintf(`{"username":"%s","password":"%s"}`, username, password)
+	return base64.URLEncoding.EncodeToString([]byte(jsonString))
+}
+
+func checkDockerCredentials() error {
+	for _, key := range []string{dockerUsernameKey, dockerPasswordKey, dockerRegistryHostKey} {
+		if value := os.Getenv(dockerUsernameKey); value == "" {
+			return fmt.Errorf("Env var %s not set", key)
+		}
 	}
 	return nil
 }
