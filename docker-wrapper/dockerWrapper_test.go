accc4c7d0a0afccfdcf2e725bf17a55347547281
reivaj05 <reivaj0705@gmail.com>
2018-01-25 16:47:18 -0600

    Add dockerWraperr test skeleton

--- docker-wrapper/dockerWrapper_test.go
+++ docker-wrapper/dockerWrapper_test.go
@@ -1,1 +1,35 @@
 package dockerWrapper
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type DockerWrapperTestSuite struct {
+	suite.Suite
+	assert *assert.Assertions
+}
+
+func (suite *DockerWrapperTestSuite) SetupSuite() {
+	suite.assert = assert.New(suite.T())
+}
+
+func (suite *DockerWrapperTestSuite) SetupTest() {
+}
+
+func (suite *DockerWrapperTestSuite) TearDownTest() {
+}
+
+// func (suite *DockerWrapperTestSuite) TestCreateGithubRepoSuccessfully() {
+// 	os.Setenv(githubKey, successfulToken)
+// 	err := CreateRepo(suite.managerName, suite.githubProvider)
+// 	suite.assert.Nil(err)
+// }
+
+func TestDockerWrapper(t *testing.T) {
+	suite.Run(t, new(DockerWrapperTestSuite))
+}
