5dcc79595d7fb73debb0c701b802853254ead1e9
reivaj05 <reivaj0705@gmail.com>
2018-01-29 16:43:02 -0600

    Pass list of services to go template

--- generator/builders/manager/managerBuilder.go
+++ generator/builders/manager/managerBuilder.go
@@ -2,20 +2,22 @@ package managerBuilder
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/reivaj05/micro-gen/generator/utils"
 )
 
 var buildFileOptions = []*utils.GenerateFileOptions{
 	utils.CreateFileOptions("docker-compose.yml", "", "docker-compose.gen", "build/",
-		"manager", true),
+		"manager", false),
 }
 
 func Build(serviceName, services string) error {
 	if err := createDirectories(serviceName); err != nil {
 		return err
 	}
-	return createService(serviceName)
+	return createService(serviceName, services)
 }
 
 func createDirectories(serviceName string) error {
@@ -28,21 +30,28 @@ func createDirectories(serviceName string) error {
 	return nil
 }
 
-func createService(serviceName string) error {
-	return generateAllFiles(serviceName)
+func createService(serviceName, services string) error {
+	return generateAllFiles(serviceName, services)
 }
 
-func generateAllFiles(serviceName string) error {
+func generateAllFiles(serviceName, services string) error {
 	for _, optionsList := range [][]*utils.GenerateFileOptions{buildFileOptions} {
-		if err := generateFilesWithOptionsList(serviceName, optionsList); err != nil {
+		if err := generateFilesWithOptionsList(serviceName, services, optionsList); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func generateFilesWithOptionsList(serviceName string, fileOptions []*utils.GenerateFileOptions) error {
+func generateFilesWithOptionsList(serviceName, services string, fileOptions []*utils.GenerateFileOptions) error {
 	for _, options := range fileOptions {
+		options.Data = struct {
+			DockerUsername string
+			Services       []string
+		}{
+			DockerUsername: os.Getenv("DOCKER_USERNAME"),
+			Services:       strings.Split(services, ","),
+		}
 		if err := utils.GenerateFile(serviceName, options); err != nil {
 			return err
 		}
