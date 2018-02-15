43c8d0ba9050949e8765aa8cfaee346bf4b63c5c
reivaj05 <reivaj0705@gmail.com>
2018-02-15 15:40:34 -0600

    Set dynamic port and fix service name in service gen file

--- generator/builders/operations/operationsBuilder.go
+++ generator/builders/operations/operationsBuilder.go
@@ -67,34 +67,28 @@ func serviceIsInDockerRegistry(repos []*GoJSON.JSONWrapper, service string) bool
 }
 
 func generateAllFiles(opName string, services []string) error {
-	for _, service := range services {
-		if err := generateFilesForService(opName, service); err != nil {
+	for index, service := range services {
+		if err := generateFilesForService(opName, service, index); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func generateFilesForService(opName, service string) error {
+func generateFilesForService(opName, service string, index int) error {
 	if err := utils.CreateDir(fmt.Sprintf("./%s/%s", opName, service)); err != nil {
 		return err
 	}
 	if err := generateDeploymentFile(opName, service); err != nil {
 		return err
 	}
-	return generateServiceFile(opName, service)
+	return generateServiceFile(opName, service, index)
 }
 
 func generateDeploymentFile(opName, service string) error {
 	options := utils.CreateFileOptions("deployment.yml", fmt.Sprintf("%s/", service),
 		"deployment.gen", "", "operations", false)
 	options.Data = createOptionsDeploymentData(service)
-	return utils.GenerateFile(opName, options)
-}
-
-func generateServiceFile(opName, service string) error {
-	options := utils.CreateFileOptions("service.yml", fmt.Sprintf("%s/", service),
-		"service.gen", "", "operations", false)
 	return utils.GenerateFile(opName, options)
 }
 
@@ -105,5 +99,22 @@ func createOptionsDeploymentData(service string) interface{} {
 	}{
 		Service:        service,
 		DockerUsername: os.Getenv("DOCKER_USERNAME"),
+	}
+}
+
+func generateServiceFile(opName, service string, index int) error {
+	options := utils.CreateFileOptions("service.yml", fmt.Sprintf("%s/", service),
+		"service.gen", "", "operations", false)
+	options.Data = createOptionsServicetData(service, index)
+	return utils.GenerateFile(opName, options)
+}
+
+func createOptionsServicetData(service string, index int) interface{} {
+	return struct {
+		Service string
+		Index   int
+	}{
+		Service: service,
+		Index:   index,
 	}
 }
