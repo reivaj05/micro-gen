b373f2e4bd8d17fd310eb73996a4a244f6a5ea5b
reivaj05 <reivaj0705@gmail.com>
2018-02-27 16:27:59 -0600

    Add continous deployment file

---
+++ cd-manager/CDManager.go
@@ -0,0 +1,45 @@
+package CDManager
+
+import (
+	"fmt"
+	"os"
+)
+
+type CDClient interface {
+	DeployService(string) error
+}
+
+var awsKey = "AWS_TOKEN"
+var gceKey = "GCE_TOKEN"
+var herokuKey = "HEROKU_TOKEN"
+
+var CDKeys = map[string]string{
+	"aws":    awsKey,
+	"gce":    gceKey,
+	"heroku": herokuKey,
+}
+
+var CDClients = map[string]func(string) CDClient{
+	"aws": NewAWSClient,
+}
+
+func ConnectWithCIProvider(serviceName, provider string) error {
+	token, err := getToken(provider)
+	if err != nil {
+		return err
+	}
+	client := CDClients[provider](token)
+	fmt.Println(fmt.Sprintf("Activating %s in %s provider", serviceName, provider))
+	return client.DeployService(serviceName)
+}
+
+func getToken(provider string) (string, error) {
+	if key, ok := CDClient[provider]; ok {
+		accessToken := os.Getenv(key)
+		if accessToken == "" {
+			return "", fmt.Errorf("%s env var does not exist", key)
+		}
+		return accessToken, nil
+	}
+	return "", fmt.Errorf("CD provider '%s' not supported", provider)
+}
